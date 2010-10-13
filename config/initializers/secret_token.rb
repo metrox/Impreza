42be3966e3f54d1e7f8398fec9be92b4c447eba0
Dawid <metrox@metrox-laptop.(none)>
2010-10-13 14:16:38 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ffa0bac67f16a980150cd48dd86d254eaf4398f9ed325aa7f47bd4d6c30f6b5b4feb620d2278694987284b17ab65c64285dc18ba59c83df70ee4028f7245e8dc'
